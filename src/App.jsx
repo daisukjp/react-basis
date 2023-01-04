abc699c49d9c39e91c9b751649f1f53c5b1d9a92
Daisuke Sato <dsato2@nait.ca>
2023-01-04 22:37:42 +0000

    Added functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-    const onClickButton = () => alert();
-    const contentStyle = {
-        color: 'blue',
-        fontSize: '18px'
+    const [num, setNum] = useState(0) ;
+    const [faceShow, setFaceShow] = useState(false);
+    
+    const onClickCountUp = () => {
+        setNum(num + 1);
     };
+    
+    const onClickShowFaceSwitch = () => {
+        setFaceShow(!faceShow);
+    };
+    
+    useEffect(() => {
+         if (num > 0) {
+        if (num % 3 === 0) {
+            faceShow || setFaceShow(true);
+        } else {
+            faceShow && setFaceShow(false);
+        }
+    }
+    }, [num]);
+    
     return (
     <>
       <h1 style={{color : 'red'}}>Hello</h1>
-      <p style={contentStyle}>How are you?</p>
-      <button onClick={onClickButton}>Button</button>
+      <ColorfulMessage color="blue">How are you</ColorfulMessage>
+      <ColorfulMessage color="pink">I'm great</ColorfulMessage>
+      <button onClick={onClickCountUp}>Count up</button>
+      <br />
+      <button onClick={onClickShowFaceSwitch}>ON/OFF</button>
+      <p>{num}</p>
+      {faceShow && <p>😱😱😱😱😱😱</p>}
+      
     </>
     );
 };
